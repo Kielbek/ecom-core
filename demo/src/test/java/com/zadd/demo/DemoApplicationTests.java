479af04d328920e91b2a30c9a96ec8c1fb7d76bf
Kielbek <mkielbasa89@gmail.com>
2024-06-19 20:34:40 +0200

    add empty project

---
+++ demo/src/test/java/com/zadd/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zadd.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
